e076a381fe0e6e679e02b6501151b3a2af1d6ec3
giovanischram <schram.giovani@gmail.com>
2024-03-23 01:42:59 -0300

    first commit

---
+++ locals.tf
@@ -0,0 +1,7 @@
+locals {
+  default_region = "us-east-1"
+  project_name   = "time-tracking-service"
+  context_name   = "Hackathon Time Tracking"
+
+  vpc_name = "tc-vpc"
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "app_database_password" {
+  type        = string
+  description = "password that hackathon service will use to connect to database"
+}
+
+variable "api_gateway_id" {
+  type        = string
+  description = "api gateway id where we should create microservices endpoints"
+}
+
+variable "vpc_link_id" {
+  type        = string
+  description = "vpc link id to be used in api gateway configuration"
+
+}
+
+variable "authorizer_id" {
+  type        = string
+  description = "API Gateway Authorizer ID"
+}
