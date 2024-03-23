37d8775e2c2b7f3bb487e84daf6cd904c91a752b
giovanischram <schram.giovani@gmail.com>
2024-03-23 05:43:30 -0300

    Changing context name.

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   default_region = "us-east-1"
   project_name   = "time-tracking-service"
-  context_name   = "Timetracking"
+  context_name   = "Time-tracking"
 
   vpc_name = "tc-vpc"
 }
